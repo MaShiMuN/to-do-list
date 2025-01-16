09855cece23f346a03322bb6313c26bcfd9f1861
Maksim <MakSimUm20072005@gmail.com>
2025-01-16 17:28:40 +0500

    sorted

---
+++ users/settings.py
@@ -0,0 +1,13 @@
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1440),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
